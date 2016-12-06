ec244a32e925397a9fa6f60ccc83fc5bac8e40bf
Kevin Harrington <mad.hephaestus@gmail.com>
2016-12-06 10:43:46 -0500

    load and scale an SVG

--- SVGExtrude.groovy
+++ SVGExtrude.groovy
@@ -1,1 +1,13 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.svg.*;
+import eu.mihosoft.vrl.v3d.Extrude;
+
+File f = ScriptingEngine
+	.fileFromGit(
+		"https://gist.github.com/1606010b686494cea33527b71c98570a.git",//git repo URL
+		"master",//branch
+		"airFoil.svg"// File from within the Git repo
+	)
+SVGLoad s = new SVGLoad(f.toURI())
+ArrayList<CSG>foil = s.extrude(10,0.01)
+
+return foil.collect{it.scale(10)}
