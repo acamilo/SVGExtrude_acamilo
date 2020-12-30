779881156022295956688613866180dca365f2d3
Kevin Harrington <mad.hephaestus@gmail.com>
2020-12-30 14:55:11 -0500

    layer splitting

--- SVGExtrude.groovy
+++ SVGExtrude.groovy
@@ -3,12 +3,18 @@ import eu.mihosoft.vrl.v3d.Extrude;
 
 File f = ScriptingEngine
 	.fileFromGit(
-		"https://gist.github.com/1606010b686494cea33527b71c98570a.git",//git repo URL
+		"https://github.com/madhephaestus/SVGBowlerExtrude.git",//git repo URL
 		"master",//branch
-		"airFoil.svg"// File from within the Git repo
+		"drawing.svg"// File from within the Git repo
 	)
 println "Extruding SVG "+f.getAbsolutePath()
-SVGLoad s = new SVGLoad(f.toURI())
-def foil = s.extrude(10,0.01)
 
-return foil.collect{it.scale(10)}
+SVGLoad s = new SVGLoad(f.toURI())
+println "Layers= "+s.getLayers()
+
+def holeParts = s.extrudeLayer(10,"holes")
+def outsideParts = s.extrudeLayer(10,"outsides")
+					.collect{it.difference(holeParts)}
+def boarderParts = s.extrudeLayer(5,"boarder")
+
+return [outsideParts,boarderParts]
